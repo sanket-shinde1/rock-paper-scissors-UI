9bc384bc84faa077b0a227d3e0e2393f6f6282f9
sanket-shinde1 <sanket.shinde@indexnine.com>
2024-01-25 11:29:29 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,53 @@
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
+
+function playGame(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "IT'S A TIE!";
+    }
+    else if(playerChoice=="rock" && computerChoice == "scissors" ||
+            playerChoice=="paper" && computerChoice == "rock" ||
+            playerChoice=="scissors" && computerChoice == "paper")
+            {
+                 result = "YOU WIN!";
+    }
+    else{
+        result="YOU LOSE!";
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+    resultDisplay.classList.remove("greenText", "redText");
+
+    display(result,resultDisplay);
+}
+
+function display(result,resultDisplay){
+
+    switch(result){
+
+        case "YOU WIN!":
+            resultDisplay.classList.add("greenText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
+            break;
+        case "YOU LOSE!":
+            resultDisplay.classList.add("redText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
+            break;
+    }
+}
+
+
